dcd1614404eace2c064cec1a4cabae176abcdec7
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2018-09-15 18:36:43 -0300

    Adding project globals

---
+++ iss_mips/globals.hpp
@@ -0,0 +1,25 @@
+#ifndef GLOBALS_HPP
+#define GLOBALS_HPP
+
+#include <cstdint>
+#include <iostream>
+
+#define MEM_SIZE 4096
+
+int32_t mem[MEM_SIZE];
+
+int32_t breg[32];
+
+uint32_t PC, ri;
+
+int32_t	opcode,
+		rs,
+		rt,
+		rd,
+		shamnt,
+		funct,
+		k16;
+
+uint32_t k26;
+
+#endif
