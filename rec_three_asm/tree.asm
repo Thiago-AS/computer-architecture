3a1f7bbb8fe3ddacc98d8fcc42f7ef22f901b795
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2018-10-01 07:55:30 -0300

    Recursive insertion in tree

--- rec_three_asm/tree.asm
+++ rec_three_asm/tree.asm
@@ -9,6 +9,8 @@
 .text
 
 print_menu:
+	addi $s2, $s2, 0
+	
 	li $v0, 4
 	la $a0, menu_question
 	syscall
@@ -33,10 +35,8 @@ user_input:
 	li $v0, 5
 	syscall
 	
-	move $s0, $v0
-	
-	beq $s0, 1, insert_value     
-	beq $s0, 4, exit      
+	beq $v0, 1, insert_value     
+	beq $v0, 4, exit      
 
 	li $v0, 4           
 	la $a0, new_line          
@@ -45,32 +45,82 @@ user_input:
 	j user_input         
 
 insert_value:
+	la $s0, ($gp)        # s0 = root address
 	li $v0, 5
 	syscall
 	
 	move $s1, $v0
 
 recursion:
-	subu $sp, $sp, 8
-	sw $gp, 0($sp)
-	sw $ra, 4($sp) 
-	lw $s0, 0($gp) #s0 = root value
+	addi $sp, $sp, -16
+	sw $s0, 0($sp)
+	sw $gp, 4($sp)
+	sw $ra, 8($sp) 
+	sw $s3, 12($sp)
+	
 
-	beqz $s0, create_node
-	ble  $s1, $s0, rec_left
-	bgt $s1, $s0, rec_right
-	addi $sp, $sp, 8
+	beqz $s2, create_node
+	
+	lw $t0, 0($s0)
+	ble  $s1, $t0, rec_left
+	bgt $s1, $t0, rec_right
 	jr $ra
 
 create_node:
 	sw $s1, 0($gp)
-	sw $s2, 4($gp)
-	sw $s3, 8($gp)
-	b exit
+	li $t0, 0
+	sw $t0, 4($gp)
+	li $t1, 0
+	sw $t1, 8($gp)
+	
+	lw $s0, 0($sp)
+	lw $gp, 4($sp)
+	lw $ra, 8($sp) 
+	lw $s3, 12($sp)
+	addi $sp, $sp, 16
+	add $s2, $s2, 1
+	jr $ra
 	
 rec_left:
-
+	la $s3, ($s0)
+	lw $s0, 4($s0)
+	bnez  $s0, l1
+	
+	lw $s0, 0($sp)
+	addi $s2, $zero, 0
+	
+l1:
+	addi $gp, $gp, 12
+	jal recursion
+	
+	sw $gp, 4($s3)
+	lw $s0, 0($sp)
+	lw $gp, 4($sp)
+	lw $ra, 8($sp) 
+	lw $s3, 12($sp)
+	addi $sp, $sp, 16
+	jr $ra
+	
+	
 rec_right:
+	la $s3, ($s0)
+	lw $s0, 8($s0)
+	bnez  $s0, l2
+	
+	lw $s0, 0($sp)
+	addi $s2, $zero, 0
+	
+l2:
+	addi $gp, $gp, 12
+	jal recursion
+	
+	sw $gp, 8($s3)
+	lw $s0, 0($sp)
+	lw $gp, 4($sp)
+	lw $ra, 8($sp) 
+	lw $s3, 12($sp)
+	addi $sp, $sp, 16
+	jr $ra
 
 exit: 
 	li $v0, 10          #Terminate Program
